48dcefa480055537f588e2ed8defa5d41264fcd6
rasagnyaa <rasagnyakannuri@gmail.com>
2024-02-11 17:58:07 +0530

    commit

---
+++ javascript/switch.js
@@ -0,0 +1,73 @@
+
+var a=0;
+switch(a){
+case 0:
+    console.log("january");
+break;
+
+case 1:
+    console.log("february");
+break;
+
+case 2:
+    console.log("march");
+break;
+
+case 3:
+    console.log("april");
+break;
+
+case 4:
+    console.log("may");
+break;
+
+
+case 5:
+    console.log("june");
+break;
+
+case 6:
+    console.log("july");
+break;
+
+case 7:
+    console.log("august");
+break;
+
+case 8:
+    console.log("september");
+break;
+
+case 9:
+    console.log("october");
+break;
+
+case 10:
+    console.log("november");
+break;
+case 11:
+    console.log("december");
+break;
+default:
+    console.log("Enter number between 0-11");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
