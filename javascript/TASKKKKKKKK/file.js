3a6f2b2d38de7f73ba419fc2cbf37fa90101ad40
rasagnyaa <rasagnyakannuri@gmail.com>
2024-02-24 00:42:09 +0530

    commit

---
+++ javascript/TASKKKKKKKK/file.js
@@ -0,0 +1,9 @@
+var tab=document.createElement("table")
+
+var trr=document.createElement("tr")
+trr.setAttribute=data[0].id
+var td1=document.createElement("td")
+
+var td2=document.createElement("td")
+
+var td3=document.createElement("td")
